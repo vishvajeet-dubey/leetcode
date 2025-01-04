c629248affff3b09aec328eb587017cc3fc61dec
vishvajeetDubey <64639869+vishvajeet-dubey@users.noreply.github.com>
2025-01-04 07:32:02 +0530

    Time: 497 ms (5.03%), Space: 0B (100%) - LeetHub

--- 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
+++ 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
@@ -1,14 +1,20 @@
 -- Write your PostgreSQL query statement below
-SELECT
-    a.machine_id,
-    AVG(b.timestamp - a.timestamp) AS processing_time
-FROM 
-    Activity a
-CROSS JOIN 
-    Activity b
-WHERE 
-    a.machine_id=b.machine_id
-    AND a.process_id = b.process_id
-    AND a.activity_type='start'
-    AND a.activity_type <> b.activity_type
-GROUP BY a.machine_id;
+SELECT machine_id, ROUND(processing_time::NUMERIC,3) as processing_time
+FROM(    
+    SELECT
+        a.machine_id,
+        AVG (b.timestamp - a.timestamp) AS processing_time
+        -- a.timestamp,
+        -- b.timestamp,
+        -- a.activity_type,
+        -- b.activity_type
+    FROM 
+        Activity a
+    CROSS JOIN 
+        Activity b
+    WHERE 
+        a.machine_id=b.machine_id
+        AND a.process_id = b.process_id
+        AND a.activity_type='start'
+        AND a.activity_type <> b.activity_type
+    GROUP BY a.machine_id)
